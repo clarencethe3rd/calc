f4404b5d80f185242c5b853ed67cb3b5e5722b98
clarencethe3rd <clarencechan524@gmail.com>
2025-07-18 16:01:20 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,16 +3,36 @@ from tkinter import*
 gui = Tk()
 gui.title("Calculator")
 gui.geometry("800x500")
-display = StringVar()
-entry = Entry(textvariable=display,width=14,font=("",26))
+display = ""
+display2 = StringVar()
+entry = Entry(textvariable=display2,width=14,font=("",26))
 entry.place(x=0,y=0)
-
+num = 0
 grid1 = Frame(gui)
 grid1.place(x=0,y=47)
 
 def press(n):
-    display +=n
+    global display,entry,display2
+    display +=str(n)
+    print(display)
+    display2.set(display)
     
+def result():
+    global display,num
+    num = eval(display)
+    display = str(num)
+    print(num)
+    display2.set(num)
+
+"""    str_num = str(num)
+    num1 = len(str_num)
+    num1-=12
+    num = round(num,num1)
+    print(num)"""
+    
+def dele():
+    global display,display2
+    print(display.rstrip(display[-1]))
 #Buttons
 
 button_0 = Button(grid1,text= "0", font=("",25),command=lambda: press(0))
@@ -25,12 +45,12 @@ button_6 = Button(grid1,text= "6", font=("",25),command=lambda: press(6))
 button_7 = Button(grid1,text= "7", font=("",25),command=lambda: press(7))
 button_8 = Button(grid1,text= "8", font=("",25),command=lambda: press(8))
 button_9 = Button(grid1,text= "9", font=("",25),command=lambda: press(9))
-button_x = Button(grid1,text= "x", font=("",25),command=lambda: press(x))
+button_x = Button(grid1,text= "x", font=("",25),command=lambda: press("*"))
 button_div = Button(grid1,text= "÷", font=("",25),command=lambda: press("/"))
 button_add = Button(grid1,text= "+", font=("",25),command=lambda: press("+"))
 button_min = Button(grid1,text= "-", font=("",25),command=lambda: press("-"))
-button_equal = Button(grid1,text= "=", font=("",25),)
-button_del = Button(grid1,text="Del", font=("",25))
+button_equal = Button(grid1,text= "=", font=("",25),command=result)
+button_del = Button(grid1,text="Del", font=("",25),command=dele)
 button_dec = Button(grid1,text=".", font=("",25),command=lambda: press("."))
 button_per = Button(grid1,text="%", font=("",25),command=lambda: press("%"))
 button_neg = Button(grid1,text="+/-", font=("",25))
@@ -57,6 +77,4 @@ button_dec.grid(column=3,row=5)
 button_neg.grid(column=2,row=1)
 button_per.grid(column=3,row=1)
 
-x=eval("4/5")
-print(x)
 gui.mainloop()
